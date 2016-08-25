2f068daf97b7152a2969c54c86d16d813a39afe3
sergiymarchuk <serega.marchuk@gmail.com>
2016-08-25 16:39:12 +0300

    Create python_mariaDB_in.py

---
+++ python_mariaDB_in.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import MySQLdb
+
+mariadb_connection = MySQLdb.connect(host="localhost",    # your host, usually localhost
+                     user="root",         # your username
+                     passwd="123456",  # your password
+                     db="serv_info")        # name of the data base
+
+cursor = mariadb_connection.cursor()
+
+#retrieving information
+some_name = 'Server'
+cursor.execute("SELECT serv_hostname_id,serv_hostname FROM serv_hostname_tab WHERE serv_hostname=%s", (some_name,))
+'''
+for first_name, last_name in cursor:
+    print("First name: {}, Last name: {}").format(serv_hostname_id,serv_hostname)
+'''
+#insert information
+try:
+    cursor.execute("INSERT INTO serv_hostname_tab (serv_hostname) VALUES (%s)", ('Maria DB'))
+except mariadb.Error as error:
+    print("Error: {}".format(error))
+
+mariadb_connection.commit()
+print "The last inserted id was: ", cursor.lastrowid
+
+mariadb_connection.close()
